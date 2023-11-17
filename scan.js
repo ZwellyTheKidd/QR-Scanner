92d27a40c64de3698cefec801c9b041befa8f837
Zwelly Sithole <87071853+ZwellyTheKidd@users.noreply.github.com>
2023-11-17 21:15:44 +0200

    scan app

--- scan.js
+++ scan.js
@@ -1,37 +1,19 @@
+// check if dom is ready
+function domReady(fn) {
+    if (document.readyState == "complete" || document.readyState == "interactive") {
+        setTimeout(fn, 1)
 
-        // check if dom is ready
-        function domReady(fn){
-            if (document.readyState=="complete" || document.readyState == "interactive") {
-                setTimeout(fn,1)
-                
-            }else{
-                document.addEventListener("DOMContentLoaded",fn)
-            }
-        }
-        domReady(function() {
-            var myqr=document.getElementById('qr-result')
-            var lastResult,countResult=0;
-
-            //if qr code found
-
-            function onScanSuccess(decodeText,decodeResult) {
-                if (decodeText !== lastResult) {
-
-                    ++countResult;
-                    lastResult=decodeText;
-
-                    alert(decodeText,decodeResult)
-
-                    myqr.innerHTML=`you scan ${countResult} : ${decodeText}`
-                    
-                }
-                
-            }
-
-            //render your camera
-            var htmlscanner = new Html5QrcodeScanner("qr-reader",{fps:10,qrbox:250})
-
-            htmlscanner.render(onScanSuccess)
-            
-        })
-   
+    } else {
+        document.addEventListener("DOMContentLoaded", fn)
+    }
+}
+domReady(function () {
+    var myqr = document.getElementById('qr-result')
+    //if qr code found
+    function onScanSuccess(decodeText) {
+        myqr.innerHTML = `<a href="${decodeText}">${decodeText}</a>`
+    }
+    //render your camera
+    var htmlscanner = new Html5QrcodeScanner("qr-reader", { fps: 10, qrbox: 250 })
+    htmlscanner.render(onScanSuccess)
+})
