c62ed4686c325e68d3d84b0bc9061adea1313bc9
Zwelly Sithole <87071853+ZwellyTheKidd@users.noreply.github.com>
2024-01-07 17:05:43 +0200

    add link

--- scan.js
+++ scan.js
@@ -20,12 +20,18 @@ domReady(function () {
 
 
 
-// ==================================
+// =====================================================================================================
+
 // init DOM elements
 const scanElment = document.getElementById('scan');
 const historyElement = document.getElementById('ht-list');
 const favoriteElement = document.getElementById('fv-list');
 const scanResult = document.getElementById('qr-result');
+const scanButton = document.getElementById('scan-btn');
+const historyBtn = document.getElementById('btn-history');
+const favoriteBtn = document.getElementById('btn-favorite');
+
+
 
 
 
@@ -37,6 +43,9 @@ function hideDivs() {
     scanElment.classList.add('hidden')
     favoriteElement.classList.add('hidden')
     scanResult.classList.add('hidden')
+    scanButton.classList.add('hidden')
+    historyBtn.classList.add('hidden')
+    favoriteBtn.classList.add('hidden')
 }
 
 // show scan
@@ -46,8 +55,9 @@ function showScan() {
         hideDivs()
         scanElment.classList.remove('hidden')
         scanResult.classList.remove('hidden')
+        historyBtn.classList.remove('hidden')
+        favoriteBtn.classList.remove('hidden')
     }, 1000); 
-  
 }
 
 
@@ -57,8 +67,9 @@ function showHistory() {
     setTimeout(function() {
         hideDivs()
         historyElement.classList.remove('hidden')
+        scanButton.classList.remove('hidden')
+        favoriteBtn.classList.remove('hidden')
     }, 1000); 
- 
 }
 
 // show favorites 
@@ -67,6 +78,8 @@ function showFavorite() {
     setTimeout(function() {
         hideDivs()
         favoriteElement.classList.remove('hidden')
+        scanButton.classList.remove('hidden')
+        historyBtn.classList.remove('hidden')
     }, 1000); 
   
 }
@@ -74,12 +87,42 @@ function showFavorite() {
 
 // run this only oce
 document.addEventListener('DOMContentLoaded', function () {
-    // hide everything
-
     // show the scan
-    showScan();
+    hideDivs()
+    scanElment.classList.remove('hidden')
+    scanResult.classList.remove('hidden')
+    historyBtn.classList.remove('hidden')
+    favoriteBtn.classList.remove('hidden')
 
 }, false);
 
 
+// =====================================================================================================
+
+
+let globalID = 0;
+
+
+// add qr link
+function addLink() {
+
+    globalID = globalID + 1
+
+    user.push({ id: globalID, type: transactionType, amount: transactionAmount, detail: transactionDetail })
+
+    saveToStorage()
+}
+
+
+
+
+function saveToStorage() {
+    localStorage.setItem('history', JSON.stringify(user))
+}
+function readFromStorage() {
+    if (localStorage.getItem('history')) {
+        state.transactions = JSON.parse(localStorage.getItem('history'))
+    }
+
+}
 
