b9b026a93b551d715d61907430c1da133da4cd6a
Zwelly Sithole <87071853+ZwellyTheKidd@users.noreply.github.com>
2024-01-08 22:58:52 +0200

    reverse history display

--- scan.js
+++ scan.js
@@ -76,7 +76,6 @@ function showHistory() {
         readFromFavorite();
         readFromStorage();
         displayHistory();
-        console.log(user);
 
     }, 1000);
 }
@@ -92,7 +91,6 @@ function showFavorite() {
 
         readFromFavorite()
         displayFav();
-        console.log(favorite);
 
     }, 1000);
 
@@ -247,11 +245,12 @@ function displayHistory() {
     <h6>-by ZWELLY SITHOLE</h6>
     </div>
 `
+   // Reverse the user array before displaying
+   const reversedUser = [...user].reverse();
 
+    for (let i = 0; i < reversedUser.length; i++) {
 
-    for (let i = 0; i < user.length; i++) {
-
-        const isInFavorite = favorite.some(link => link.id === user[i].id);
+        const isInFavorite = favorite.some(link => link.id === reversedUser[i].id);
 
         linkHistory.innerHTML += `
         <div class="link-box">
@@ -265,24 +264,24 @@ function displayHistory() {
                 </svg>
 
                 <div>
-                ${isInFavorite ? (favorite.find(link => link.id === user[i].id)?.web || user[i].web) : user[i].web}
+                ${isInFavorite ? (favorite.find(link => link.id === reversedUser[i].id)?.web || reversedUser[i].web) : reversedUser[i].web}
             </div>
 
             </div>
             <div class="date" id="date">
-            ${user[i].date}
+            ${reversedUser[i].date}
             </div>
             <div class="link" id="link">
-                ${user[i].link}
+                ${reversedUser[i].link}
                 <div class="opt-icon">
                 
-                    <svg onclick="addFavorite(${user[i].id})" id="starIcon-${user[i].id}"  style="width: 1.5em;height:1.5em;cursor: pointer" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                    <svg onclick="addFavorite(${reversedUser[i].id})" id="starIcon-${reversedUser[i].id}"  style="width: 1.5em;height:1.5em;cursor: pointer" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                         fill="currentColor" class="bi bi-star ${isInFavorite ? 'favorited' : ''}" viewBox="0 0 16 16">
                         <path
                             d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
                     </svg>
 
-                    <svg onclick="removeLink(${user[i].id})" style="width: 1.5em;height:1.5em;cursor: pointer" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                    <svg onclick="removeLink(${reversedUser[i].id})" style="width: 1.5em;height:1.5em;cursor: pointer" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                         fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16" >
                         <path
                             d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
